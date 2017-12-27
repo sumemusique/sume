bc27d61e7711ab06b1adbe7a16e1c1d35473dc51
Fábio Duarte <sumeproject@gmail.com>
2017-12-27 13:33:20 -0200

    image new

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,15 @@
 		xxsmall:	'(max-width: 360px)'
 	});
 
+
+[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function() {
+    img.removeAttribute('data-src');
+  };
+});
+
+
 	$(function() {
 
 		var	$window = $(window),
