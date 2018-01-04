1c877c6ff0215067f770e1788059994d98a0e5d0
Fábio Duarte <sumeproject@gmail.com>
2018-01-04 18:04:49 -0200

    shiieeet

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,15 +16,16 @@
 	});
 
 
-[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+/*		[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
   img.setAttribute('src', img.getAttribute('data-src'));
   img.onload = function() {
     img.removeAttribute('data-src');
   };
-});
+});*/
 
 
 	$(function() {
+
 
 		var	$window = $(window),
 			$body = $('body'),
